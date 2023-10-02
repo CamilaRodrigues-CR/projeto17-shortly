33c6959f1ef0c808778180113aab8eb27a514728
Driven Education <camilarodriguescrs13@gmail.com>
2023-10-02 15:59:13 -0300

    requisito ranking correção 2

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -11,7 +11,7 @@ export async function getUsers(req, res) {
         const userId = await db.query(`SELECT "userId" FROM sessions WHERE token = $1;`, [session.rows[0].token]);
 
         //preciso fazer um join do usuario com todos os seus links
-        const user_url = await db.query(`SELECT users.id, users.name, urls.* FROM users JOIN urls ON users.id = urls."createdByUserId" WHERE users.id = $1;`, [userId.rows[0].userId])
+        const user_url = await db.query(`SELECT users.id AS "userId", users.name, urls.id AS "urlId", urls.url, urls."shortUrl", urls.visits FROM users JOIN urls ON users.id = urls."createdByUserId" WHERE users.id = $1;`, [userId.rows[0].userId])
 
         // preciso somar quantas visitas totais esse usuario tem
         const TotalvisitCount = await db.query(`SELECT SUM(visits) AS "visitCount" FROM urls WHERE "createdByUserId" = $1 GROUP BY "createdByUserId";`, [userId.rows[0].userId])
@@ -20,10 +20,12 @@ export async function getUsers(req, res) {
         //(usar o reduce ou invés de map para ter o retorno no formato de objeto e não de array!!!)
         const result = user_url.rows.reduce((acc, u) => {
             const obj = {
+                id: userId.rows[0].userId,
                 ...u,
                 visitCount: TotalvisitCount.rows[0].visitCount,
                 shortenedUrls: [
                     {
+                        id: u.urlId,
                         shortUrl: u.shortUrl,
                         url: u.url,
                         visitCount: u.visits
@@ -31,7 +33,8 @@ export async function getUsers(req, res) {
                     }
                 ]
             }
-
+            delete obj.userId;
+            delete obj.urlId;
             delete obj.url;
             delete obj.shortUrl;
             delete obj.createdByUserId;
